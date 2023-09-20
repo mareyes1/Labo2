66bb16a01eef54b17f2851323bb059ef6e5ee980
mareyes1 <reyesmejias@gmail.com>
2023-09-19 23:46:57 -0600

    Monitorear uso de recursos y crear logfile

---
+++ enunciado3.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]; then #solo permite 1 parámetro pero podría hacerse con más
+	echo "Uso: $0 <path a un ejecutable>" #se prueba con script de sleep
+	exit 1
+fi
+
+ejecutable=$1 #path del ejecutable pasado
+logfile="uso_cpu_mem.log" #archivo de log de uso de cpu y memoria
+periodicidad=1 #intervalo de muestreo para el log, menor para mejor gráfica
+$ejecutable & #ejecutar el ejecutable en el background mediante &, se debe matar sleep de prueba luego
+pid=$! #obtener PID del proceso recién ejecutado usando variable $!
+echo "HH:MM:SS %CPU %MEM" > "$logfile" #encabezado para el log, uso de recursos y hora exacta
+
+while ps -p $pid > /dev/null; do #mientras el proceso exista se ejecuta ciclo infinito
+	tiempo=$(date +"%H:%M:%S") #date sugerido en enunciado da hora exacta
+	uso_cpu=$(ps -o %cpu= -p $pid) 
+	uso_mem=$(ps -o %mem= -p $pid)
+	echo "$tiempo $uso_cpu $uso_mem" >> "$logfile"
+	sleep $periodicidad
+done
---
+++ prueba_enunciado3.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+while true; do
+	echo "Ejecutable para prueba del enunciado #3 - debe matar este proceso..."
+	sleep 1
+done
